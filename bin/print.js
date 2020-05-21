f85d717e0ddb7f33879f24710fd71e09ef13cfe4
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-21 11:25:15 +0200

    build: add console.log for terImg

--- bin/print.js
+++ bin/print.js
@@ -32,14 +32,14 @@ module.exports = {
   },
 
   image: filepath =>
-    termImg(filepath, {
-      fallback: () =>
-        console.log(
+    console.log(
+      termImg(filepath, {
+        fallback: () =>
           `\n${chalk.yellow('  tip:')} ${chalk.gray(
             'use iTerm >=3 to see the image here!'
           )}`
-        )
-    }),
+      })
+    ),
 
   link: (text, url) => terminalLink(text, url, { fallback: () => url })
 }
