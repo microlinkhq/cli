53305f1a1c2437f9476094ca959f340161450744
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-05-03 00:15:50 +0200

    fix: flag types

--- src/cli.js
+++ src/cli.js
@@ -3,22 +3,12 @@
 const mri = require('mri')
 
 const { _, ...flags } = mri(process.argv.slice(2), {
+  boolean: ['color', 'copy', 'pretty'],
   default: {
-    apiKey: {
-      default: process.env.MICROLINK_API_KEY
-    },
-    pretty: {
-      type: 'boolean',
-      default: true
-    },
-    color: {
-      type: 'boolean',
-      default: true
-    },
-    copy: {
-      type: 'boolean',
-      default: false
-    }
+    apiKey: process.env.MICROLINK_API_KEY,
+    pretty: true,
+    color: true,
+    copy: false
   }
 })
 
